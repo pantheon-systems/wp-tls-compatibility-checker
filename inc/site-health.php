39f1f82d2061aba3c0f626885f8de1661cfd963a
Chris Reynolds <chris@jazzsequence.com>
2025-01-29 09:54:54 -0700

    refactor and fix site-health test result

--- inc/site-health.php
+++ inc/site-health.php
@@ -18,23 +18,28 @@ function site_health_check( $tests ) {
 
 function site_health_test() {
 	$failing_urls = pantheon_tls_checker_get_failing_urls();
-
-	if ( ! empty( $failing_urls ) ) {
-		return [
-			'status' => 'critical',
-			'label' => __( 'TLS Failing URLs Detected', 'pantheon-tls-compatibility-checker' ),
-			'message' => sprintf(
-				__( 'The following URLs do not support TLS 1.2 or higher: %s', 'pantheon-tls-compatibility-checker' ),
-				'<ul><li>' . implode( '</li><li>', array_map( 'esc_html', $failing_urls ) ) . '</li></ul>'
-			),
-		];
-	}
-
-	return [
+	$result = [
 		'status' => 'good',
 		'label' => __( 'All scanned URLs pass TLS check', 'pantheon-tls-compatibility-checker' ),
-		'message' => __( 'All scanned URLs support TLS 1.2 or higher.', 'pantheon-tls-compatibility-checker' ),
+		'description' => __( 'All scanned URLs support TLS 1.2 or higher.', 'pantheon-tls-compatibility-checker' ),
+		'badge' => [
+			'label' => __( 'TLS Compatibility', 'pantheon-tls-compatibility-checker' ),
+			'color' => 'green',
+		],
+		'test' => 'pantheon_tls_check_failing_urls',
 	];
+
+	if ( ! empty( $failing_urls ) ) {
+		$result['status'] = 'critical';
+		$result['label'] = __( 'TLS Failing URLs Detected', 'pantheon-tls-compatibility-checker' );
+		$result['badge']['color'] = 'red';
+		$result['description'] = sprintf(
+				__( 'The following URLs do not support TLS 1.2 or higher: %s', 'pantheon-tls-compatibility-checker' ),
+				'<ul><li>' . implode( '</li><li>', array_map( 'esc_html', $failing_urls ) ) . '</li></ul>'
+		);
+	}
+
+	return $result;
 }
 
 add_filter( 'site_status_tests', __NAMESPACE__ . '\\site_health_check', 10 );
