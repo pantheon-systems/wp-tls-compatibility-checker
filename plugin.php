98c7f9504aa9b4827a1c2f0b30d4624727376233
Chris Reynolds <chris@jazzsequence.com>
2025-01-29 15:37:33 -0700

    add assets and version constants

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ namespace Pantheon\TLSChecker;
 
 function bootstrap() {
 	define( 'TLS_CHECKER_INC', plugin_dir_path( __FILE__ ) . '/inc/' );
+	define( 'TLS_CHECKER_ASSETS', plugin_dir_path( __FILE__ ). '/assets/' );
+	define( 'TLS_CHECKER_VERSION', '1.3' );
 	require_once TLS_CHECKER_INC . 'core.php';
 	require_once TLS_CHECKER_INC . 'admin.php';
 	require_once TLS_CHECKER_INC . 'site-health.php';
